24cb6155f407b66c5a71a2682733b04f71b5cc2b
WayanPande <yande554@gmail.com>
2021-03-17 13:26:59 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("toastbtn").onclick = function() {
+    var myAlert =document.getElementById('toastNotice');//select id of toast
+    var bsAlert = new bootstrap.Toast(myAlert);//inizialize it
+    bsAlert.show();//show it
+}
